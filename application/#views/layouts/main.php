59222166cd5eb802d57acc7e005420d09ac5ec76
AnnisaBaizan <annisabaizan1@gmail.com>
2025-10-31 13:26:49 +0700

    add SIMKESGI button to navbar

--- application/#views/layouts/main.php
+++ application/#views/layouts/main.php
@@ -5,45 +5,45 @@
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <title>Rekam Medik Gigi</title>
         <!-- Favicon-->
-        <!--<link rel="icon" href="<?php echo site_url('resources/img/favicon.ico');?>" type="image/x-icon">-->
+        <!--<link rel="icon" href="<?php echo site_url('resources/img/favicon.ico'); ?>" type="image/x-icon">-->
 
         <!-- Google Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
-		<!--<link href="<?php echo site_url('resources/css/icon.css');?>" rel="stylesheet" type="text/css">-->
-		
+        <!--<link href="<?php echo site_url('resources/css/icon.css'); ?>" rel="stylesheet" type="text/css">-->
+        
         <!-- Font-awesome Css -->
-        <link href="<?php echo site_url('resources/css/font-awesome.min.css');?>" rel="stylesheet">
+        <link href="<?php echo site_url('resources/css/font-awesome.min.css'); ?>" rel="stylesheet">
 
         <!-- Bootstrap Core Css -->
-        <link href="<?php echo site_url('resources/css/bootstrap.css');?>" rel="stylesheet">
+        <link href="<?php echo site_url('resources/css/bootstrap.css'); ?>" rel="stylesheet">
 
         <!-- Waves Effect Css -->
-        <link href="<?php echo site_url('resources/css/waves.css');?>" rel="stylesheet" />
+        <link href="<?php echo site_url('resources/css/waves.css'); ?>" rel="stylesheet" />
 
         <!-- Animation Css -->
-        <link href="<?php echo site_url('resources/css/animate.css');?>" rel="stylesheet" />
+        <link href="<?php echo site_url('resources/css/animate.css'); ?>" rel="stylesheet" />
 
         <!-- Preloader Css -->
-        <link href="<?php echo site_url('resources/css/md-preloader.css');?>" rel="stylesheet" />
+        <link href="<?php echo site_url('resources/css/md-preloader.css'); ?>" rel="stylesheet" />
 
         <!-- Bootstrap Material Datetime Picker Css -->
-        <link href="<?php echo site_url('resources/css/bootstrap-material-datetimepicker.css');?>" rel="stylesheet" />
+        <link href="<?php echo site_url('resources/css/bootstrap-material-datetimepicker.css'); ?>" rel="stylesheet" />
 
         <!-- Wait Me Css -->
-        <link href="<?php echo site_url('resources/css/waitMe.css');?>" rel="stylesheet" />
+        <link href="<?php echo site_url('resources/css/waitMe.css'); ?>" rel="stylesheet" />
 
         <!-- Bootstrap Select Css -->
-        <link href="<?php echo site_url('resources/css/bootstrap-select.css');?>" rel="stylesheet" />
+        <link href="<?php echo site_url('resources/css/bootstrap-select.css'); ?>" rel="stylesheet" />
 
         <!-- Custom Css -->
-        <link href="<?php echo site_url('resources/css/style.css');?>" rel="stylesheet">
+        <link href="<?php echo site_url('resources/css/style.css'); ?>" rel="stylesheet">
 
         <!-- AdminBSB Themes. You can choose a theme from css/themes instead of get all themes -->
-        <link href="<?php echo site_url('resources/css/all-themes.css');?>" rel="stylesheet" />
-		
-		<!-- dataTable -->
-		<link href="<?php echo site_url('resources/css/jquery.dataTables.min.css');?>" type="text/css" rel="stylesheet" media="screen,projection">
+        <link href="<?php echo site_url('resources/css/all-themes.css'); ?>" rel="stylesheet" />
+        
+        <!-- dataTable -->
+        <link href="<?php echo site_url('resources/css/jquery.dataTables.min.css'); ?>" type="text/css" rel="stylesheet" media="screen,projection">
     </head>
 
     <body class="theme-red">
@@ -72,6 +72,16 @@
                     <a class="navbar-brand" href="">Sistem Penilaian Praktek Gigi</a>
                 </div>
                 <div class="collapse navbar-collapse" id="navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="https://simkesgi.poltekkespalembang.ac.id"
+                       target="_blank"
+                       class="btn bg-blue waves-effect waves-light btn-lg">
+                        <i class="material-icons">medical_services</i>
+                        <span><strong>SIMKESGI</strong></span>
+                    </a>
+                </li>
+            </ul>
             </div>
             </div>
         </nav>
@@ -82,11 +92,11 @@
                 <!-- User Info -->
                 <div class="user-info">
                     <div class="image">
-                        <img src="<?php echo site_url('resources/img/user.png');?>" width="48" height="48" alt="User" />
+                        <img src="<?php echo site_url('resources/img/user.png'); ?>" width="48" height="48" alt="User" />
                     </div>
                     <div class="info-container">
-                        <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo strtoupper($_SESSION['username_admin']);?></div>
-                        <div class="email"><?php echo ($_SESSION['level_admin'] = 'su' ? 'Super User' : 'Operator');?></div>
+                        <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo strtoupper($_SESSION['username_admin']); ?></div>
+                        <div class="email"><?php echo ($_SESSION['level_admin'] = 'su' ? 'Super User' : 'Operator'); ?></div>
                         
                     </div>
                 </div>
@@ -96,74 +106,74 @@
                     <ul class="list">
                         <li class="header">MAIN NAVIGATION </li>
 
-						<?php echo ($this->uri->segment(1)=='mahasiswa' ? "<li class='active'>" : "<li>"); ?>
-                            <a href="<?php echo site_url('mahasiswa/index');?>">
+                        <?php echo ($this->uri->segment(1) == 'mahasiswa' ? "<li class='active'>" : "<li>"); ?>
+                            <a href="<?php echo site_url('mahasiswa/index'); ?>">
                                 <i class="material-icons">supervisor_account</i>
                                 <span>Mahasiswa</span>
                             </a>
                         </li>
-						<?php echo ($this->uri->segment(1)=='dosen' ? "<li class='active'>" : "<li>"); ?>
-                            <a href="<?php echo site_url('dosen/index');?>">
+                        <?php echo ($this->uri->segment(1) == 'dosen' ? "<li class='active'>" : "<li>"); ?>
+                            <a href="<?php echo site_url('dosen/index'); ?>">
                                 <i class="material-icons">supervisor_account</i>
                                 <span>Dosen</span>
                             </a>
                         </li>
-						<?php echo ($this->uri->segment(1)=='pasien' ? "<li class='active'>" : "<li>"); ?>
-                            <a href="<?php echo site_url('pasien/index');?>">
+                        <?php echo ($this->uri->segment(1) == 'pasien' ? "<li class='active'>" : "<li>"); ?>
+                            <a href="<?php echo site_url('pasien/index'); ?>">
                                 <i class="material-icons">person_pin</i>
                                 <span>Pasien</span>
                             </a>
                         </li>						
-						<?php echo ($this->uri->segment(1)=='konfigurasi' ? "<li class='active'>" : "<li>"); ?>
-                            <a href="<?php echo site_url('konfigurasi/index');?>">
+                        <?php echo ($this->uri->segment(1) == 'konfigurasi' ? "<li class='active'>" : "<li>"); ?>
+                            <a href="<?php echo site_url('konfigurasi/index'); ?>">
                                 <i class="material-icons">mode_edit</i>
                                 <span>Konfigurasi</span>
                             </a>
                         </li>
-						<?php echo ($this->uri->segment(1)=='nilai_praktek' ? "<li class='active'>" : "<li>"); ?>
-                            <a href="<?php echo site_url('nilai_praktek/index');?>">
+                        <?php echo ($this->uri->segment(1) == 'nilai_praktek' ? "<li class='active'>" : "<li>"); ?>
+                            <a href="<?php echo site_url('nilai_praktek/index'); ?>">
                                 <i class="material-icons">assessment</i>
                                 <span>Nilai Praktek</span>
                             </a>
                         </li>
-						<?php echo ($this->uri->segment(1)=='praktikum' ? "<li class='active'>" : "<li>"); ?>
-                            <a href="<?php echo site_url('praktikum/index');?>">
+                        <?php echo ($this->uri->segment(1) == 'praktikum' ? "<li class='active'>" : "<li>"); ?>
+                            <a href="<?php echo site_url('praktikum/index'); ?>">
                                 <i class="material-icons">view_list</i>
                                 <span>Praktikum</span>
                             </a>
                         </li>
-						<?php echo ($this->uri->segment(1)=='sub_praktikum' ? "<li class='active'>" : "<li>"); ?>
-                            <a href="<?php echo site_url('sub_praktikum/index');?>">
+                        <?php echo ($this->uri->segment(1) == 'sub_praktikum' ? "<li class='active'>" : "<li>"); ?>
+                            <a href="<?php echo site_url('sub_praktikum/index'); ?>">
                                 <i class="material-icons">view_list</i>
                                 <span>Sub Praktikum</span>
                             </a>
                         </li>
-						<?php echo ($this->uri->segment(1)=='aspek' ? "<li class='active'>" : "<li>"); ?>
-                            <a href="<?php echo site_url('aspek/index');?>">
+                        <?php echo ($this->uri->segment(1) == 'aspek' ? "<li class='active'>" : "<li>"); ?>
+                            <a href="<?php echo site_url('aspek/index'); ?>">
                                 <i class="material-icons">subject</i>
                                 <span>Aspek</span>
                             </a>
                         </li>
-						<?php echo ($this->uri->segment(1)=='sub_aspek' ? "<li class='active'>" : "<li>"); ?>
-                            <a href="<?php echo site_url('sub_aspek/index');?>">
+                        <?php echo ($this->uri->segment(1) == 'sub_aspek' ? "<li class='active'>" : "<li>"); ?>
+                            <a href="<?php echo site_url('sub_aspek/index'); ?>">
                                 <i class="material-icons">subject</i>
                                 <span>Sub Aspek</span>
                             </a>
                         </li>
-						<?php echo ($this->uri->segment(1)=='admin' ? "<li class='active'>" : "<li>"); ?>
-                            <a href="<?php echo site_url('admin/index');?>">
+                        <?php echo ($this->uri->segment(1) == 'admin' ? "<li class='active'>" : "<li>"); ?>
+                            <a href="<?php echo site_url('admin/index'); ?>">
                                 <i class="material-icons">perm_identity</i>
                                 <span>Admin</span>
                             </a>
                         </li>
-						<?php echo ($this->uri->segment(1)=='password_admin' ? "<li class='active'>" : "<li>"); ?>
-                            <a href="<?php echo site_url('password_admin/index');?>">
+                        <?php echo ($this->uri->segment(1) == 'password_admin' ? "<li class='active'>" : "<li>"); ?>
+                            <a href="<?php echo site_url('password_admin/index'); ?>">
                                 <i class="material-icons">lock</i>
                                 <span>Ubah Password</span>
                             </a>
                         </li>
-						<?php echo ($this->uri->segment(1)=='login' ? "<li class='active'>" : "<li>"); ?>
-                            <a href="<?php echo site_url('login');?>">
+                        <?php echo ($this->uri->segment(1) == 'login' ? "<li class='active'>" : "<li>"); ?>
+                            <a href="<?php echo site_url('login'); ?>">
                                 <i class="material-icons">input</i>
                                 <span>Keluar</span>
                             </a>
@@ -184,48 +194,48 @@
         <section class="content">
             <div class="container-fluid">
                 <div class="block-header">
-                    <h2><?php echo "$judulform";?></h2>
+                    <h2><?php echo "$judulform"; ?></h2>
                 </div>
-                <?php                    
-                if(isset($_view) && $_view)
+                <?php
+                if (isset($_view) && $_view)
                     $this->load->view($_view);
                 ?>                
             </div>
         </section>
 
         <!-- Jquery Core Js -->
-        <script src="<?php echo site_url('resources/js/jquery.min.js');?>"></script>
+        <script src="<?php echo site_url('resources/js/jquery.min.js'); ?>"></script>
 
         <!-- Bootstrap Core Js -->
-        <script src="<?php echo site_url('resources/js/bootstrap.js');?>"></script>
+        <script src="<?php echo site_url('resources/js/bootstrap.js'); ?>"></script>
 
         <!-- Select Plugin Js -->
-        <script src="<?php echo site_url('resources/js/bootstrap-select.js');?>"></script>
+        <script src="<?php echo site_url('resources/js/bootstrap-select.js'); ?>"></script>
 
         <!-- Slimscroll Plugin Js -->
-        <script src="<?php echo site_url('resources/js/jquery.slimscroll.js');?>"></script>
+        <script src="<?php echo site_url('resources/js/jquery.slimscroll.js'); ?>"></script>
 
         <!-- Waves Effect Plugin Js -->
-        <script src="<?php echo site_url('resources/js/waves.js');?>"></script>
+        <script src="<?php echo site_url('resources/js/waves.js'); ?>"></script>
 
         <!-- Autosize Plugin Js -->
-        <script src="<?php echo site_url('resources/js/autosize.js');?>"></script>
+        <script src="<?php echo site_url('resources/js/autosize.js'); ?>"></script>
 
         <!-- Moment Plugin Js -->
-        <script src="<?php echo site_url('resources/js/moment.js');?>"></script>
+        <script src="<?php echo site_url('resources/js/moment.js'); ?>"></script>
 
         <!-- Bootstrap Material Datetime Picker Plugin Js -->
-        <script src="<?php echo site_url('resources/js/bootstrap-material-datetimepicker.js');?>"></script>
+        <script src="<?php echo site_url('resources/js/bootstrap-material-datetimepicker.js'); ?>"></script>
 
         <!-- Custom Js -->
-        <script src="<?php echo site_url('resources/js/admin.js');?>"></script>
-        <script src="<?php echo site_url('resources/js/basic-form-elements.js');?>"></script>
+        <script src="<?php echo site_url('resources/js/admin.js'); ?>"></script>
+        <script src="<?php echo site_url('resources/js/basic-form-elements.js'); ?>"></script>
 
         <!-- Demo Js -->
-        <script src="<?php echo site_url('resources/js/demo.js');?>"></script>
-		
-		<!-- data-tables -->
-		<script type="text/javascript" src="<?php echo site_url('resources/js/jquery.dataTables.min.js');?>"></script>
-		<script type="text/javascript" src="<?php echo site_url('resources/js/data-tables-script.js');?>"></script>
+        <script src="<?php echo site_url('resources/js/demo.js'); ?>"></script>
+        
+        <!-- data-tables -->
+        <script type="text/javascript" src="<?php echo site_url('resources/js/jquery.dataTables.min.js'); ?>"></script>
+        <script type="text/javascript" src="<?php echo site_url('resources/js/data-tables-script.js'); ?>"></script>
     </body>
 </html>
